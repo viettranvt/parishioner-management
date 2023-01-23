c97f48bb945ee36edfce09551a8e09568dcbc0bd
Tue Vo <vothaiminhtue0312@gmail.com>
2023-01-23 22:22:36 +0700

    init client project

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { MainLayout } from 'components/MainLayout/MainLayout';
+import './App.css';
+
+function App() {
+   return (
+      <div className="App">
+         <MainLayout />
+      </div>
+   );
+}
+
+export default App;
---
+++ client/src/constants/app-config.ts
@@ -0,0 +1,4 @@
+export const AppConfig = {
+    appName: 'QUẢN LÝ GIÁO DÂN',
+    apiEndpoint: process.env.REACT_APP_API_ENDPOINT,
+};
